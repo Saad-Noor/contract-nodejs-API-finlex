20ff766e98f98ccb4253feec8e7b71bd7676787d
Saad-Noor <saadnoor711@gmail.com>
2020-06-16 22:42:30 +0200

    Changed port

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? ' https://finlex-backend.herokuapp.com/ '
+  ? ' https://finlex-backend.herokuapp.com '
   : 'http://localhost:4200'
